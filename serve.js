fe73a0aa8f66db5e12f99ddc25fde641100b2510
Frantisek Psotka <frantisek.psotka@stylez.sk>
2025-11-18 14:25:55 +0100

    port 5002

--- serve.js
+++ serve.js
@@ -1,14 +1,14 @@
 // start local static server to serve extension
 
-const package = require('./package.json');
-const spawn = require('child_process').spawn;
+const package = require("./package.json");
+const spawn = require("child_process").spawn;
 
-const PORT = '5001';
+const PORT = "5002";
 
-spawn('sirv', ['dist', '-p', PORT, '--cors', '--quiet', '--dev']);
+spawn("sirv", ["dist", "-p", PORT, "--cors", "--quiet", "--dev"]);
 
 console.log(
-  '\x1b[36m%s\x1b[0m',
+  "\x1b[36m%s\x1b[0m",
   `+------------------------------------------------------------------------+
 
    🍕 Extension URL: http://localhost:${PORT}/${package.name}.umd.js
